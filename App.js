1fed4a81e8c07fb20ebc251644265db819853543
Polina Filina <polinafilina@Polinas-MacBook-Air.local>
2025-11-16 17:53:13 +0900

    attempt to fix white screen issue

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { SQLiteProvider } from 'expo-sqlite';
